4d4baa707c057a44958c9138c0d8622e8abb6c62
Alexander Khramov <alx.khramov@gmail.com>
2022-11-07 15:43:50 +0300

    build: add @rollup/plugin-json

--- packages/connect-wallet-modal/rollup.config.js
+++ packages/connect-wallet-modal/rollup.config.js
@@ -1,21 +1,22 @@
-import fs from 'fs'
-import ts from 'typescript'
-import tslib from 'tslib'
-import del from 'rollup-plugin-delete'
-import resolve from '@rollup/plugin-node-resolve'
-import typescript from 'rollup-plugin-typescript2'
-import commonjs from '@rollup/plugin-commonjs'
-import { babel } from '@rollup/plugin-babel'
+import fs from 'fs';
+import ts from 'typescript';
+import tslib from 'tslib';
+import del from 'rollup-plugin-delete';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from '@rollup/plugin-commonjs';
+import { babel } from '@rollup/plugin-babel';
+import json from '@rollup/plugin-json';
 
-const extensions = ['.js', '.jsx', '.ts', '.tsx']
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
-const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf-8'))
-const { dependencies, peerDependencies } = packageJson
-const commonExternal = ['react/jsx-runtime']
+const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+const { dependencies, peerDependencies } = packageJson;
+const commonExternal = ['react/jsx-runtime'];
 const external = [
   ...commonExternal,
   ...Object.keys({ ...dependencies, ...peerDependencies }),
-]
+];
 
 export default {
   input: './src/index',
@@ -31,7 +32,7 @@ export default {
       dir: 'dist/esm',
       preserveModules: true,
       exports: 'named',
-    }
+    },
   ],
   plugins: [
     del({ targets: 'dist/*', runOnce: true }),
@@ -45,10 +46,10 @@ export default {
           paths: { tslib: [require.resolve('tslib/tslib.d.ts')] },
           emitDeclarationOnly: false,
           noEmit: false,
-          rootDir: './src'
+          rootDir: './src',
         },
         exclude: ['node_modules', 'dist', '**/*.stories.*', '**/*.test.*'],
-        include: ["**/src/*.ts", "**/src/*.tsx"],
+        include: ['**/src/*.ts', '**/src/*.tsx'],
       },
     }),
     commonjs(),
@@ -57,6 +58,7 @@ export default {
       babelHelpers: 'bundled',
       extensions,
     }),
+    json(),
   ],
-  external
+  external,
 };
