360b48e05af85827c19be4144517165910558e36
Alexander Khramov <alx.khramov@gmail.com>
2022-12-07 22:53:30 +0300

    chore: update package.json files

--- packages/reef-knot/src/index.ts
+++ packages/reef-knot/src/index.ts
@@ -1,2 +1,4 @@
 export * from '@reef-knot/connect-wallet-modal';
 export * from '@reef-knot/web3-react';
+export * as uiReact from '@reef-knot/ui-react';
+export * as walletsIcons from '@reef-knot/wallets-icons';
