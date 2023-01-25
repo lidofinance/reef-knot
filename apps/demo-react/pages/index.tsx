9979739bef2db2309bf6524e2b1758d9e908a163
Alexander Khramov <alx.khramov@gmail.com>
2023-01-25 23:04:49 +0300

    chore(demo-react): add theme switching

--- apps/demo-react/pages/index.tsx
+++ apps/demo-react/pages/index.tsx
@@ -1,6 +1,12 @@
 import dynamic from 'next/dynamic';
 import { WalletsModalForEth } from 'reef-knot';
-import { themeLight, ThemeProvider } from '@lidofinance/lido-ui';
+import {
+  themeLight,
+  themeDark,
+  ThemeProvider,
+  ThemeName,
+} from '@lidofinance/lido-ui';
+import { useState } from 'react';
 import Header from '../components/Header';
 import ProviderWeb3WithProps from '../components/ProviderWeb3WithProps';
 import WalletInfo from '../components/WalletInfo';
@@ -8,19 +14,30 @@ import useModal from '../hooks/useModal';
 import metrics from '../util/metrics';
 import MainContainer from '../components/MainContainer';
 import ConnectDisconnect from '../components/ConnectDisconnect';
+import ThemeSelect from '../components/ThemeSelect';
+import SettingsWrapper from '../components/SettingsWrapper';
 
 export function Web() {
   const { state, handleOpen, handleClose } = useModal();
+  const [selectedTheme, setSelectedTheme] = useState('light' as ThemeName);
 
   return (
     <>
       <Header />
-      <ThemeProvider theme={themeLight}>
+      <ThemeProvider
+        theme={selectedTheme === ThemeName.light ? themeLight : themeDark}
+      >
         <ProviderWeb3WithProps>
           <MainContainer>
             <ConnectDisconnect handleOpen={handleOpen} />
 
-            <WalletInfo />
+            <SettingsWrapper>
+              <ThemeSelect
+                selectedTheme={selectedTheme}
+                handleSelect={setSelectedTheme}
+              />
+              <WalletInfo />
+            </SettingsWrapper>
 
             <WalletsModalForEth
               open={state}
