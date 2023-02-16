737127e5df8ed3af3f04e88759ccab46541c615a
Alexander Khramov <alx.khramov@gmail.com>
2023-02-16 12:25:12 +0300

    chore(demo-react): rm unneeded code

--- apps/demo-react/next.config.js
+++ apps/demo-react/next.config.js
@@ -1,7 +1,3 @@
-import { createRequire } from 'node:module';
-
-const require = createRequire(import.meta.url);
-
 export default {
   reactStrictMode: true,
   basePath: process.env.BASE_PATH || '',
@@ -9,34 +5,5 @@ export default {
     styledComponents: {
       ssr: true
     }
-  },
-  webpack(config, { webpack }) {
-    // Related:
-    // - https://github.com/WalletConnect/walletconnect-monorepo/issues/584
-    // - https://webpack.js.org/configuration/resolve/#resolvefallback
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      tls: false,
-      net: false,
-      worker_threads: false,
-      module: false,
-      util: require.resolve('util'),
-      url: require.resolve('url'),
-      buffer: require.resolve('buffer')
-    };
-
-    config.resolve.alias = {
-      ...config.resolve.alias,
-    // your aliases
-    }
-
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        Buffer: ['buffer', 'Buffer'],
-      }),
-    );
-
-    return config;
   },
 }
--- apps/demo-react/util/polyfills.ts
+++
@@ -1,9 +0,0 @@
-import { Buffer } from 'buffer';
-
-if (typeof window !== 'undefined') {
-  window.global = window.global ?? window;
-  window.Buffer = window.Buffer ?? Buffer;
-  window.process = window.process ?? { env: {} }; // Minimal process polyfill
-}
-
-export {};
