7efb467e80282cf25ade7ed11662babf9c9877aa
Alexander Khramov <alx.khramov@gmail.com>
2023-03-02 13:39:44 +0300

    chore: WalletAdapterData: add icons, downloadURLs fields with comments

--- packages/core-react/src/types/index.ts
+++ packages/core-react/src/types/index.ts
@@ -3,8 +3,25 @@ import { ElementType } from 'react';
 export type WalletAdapterData = {
   walletId: string;
   walletName: string;
-  icon: ElementType;
+
+  // Icons for the light and dark color themes.
+  // You can use different icons or the same for both cases.
+  icons: {
+    light: ElementType;
+    dark: ElementType;
+  };
+
+  // A function to check if the wallet is installed and injected.
+  // For example: isMetaMaskProvider: () => !!window.ethereum?.isMetaMask
   detector: () => boolean;
+
+  // URL to redirect a user if the Wallet Button was clicked,
+  // but the wallet is not installed or active
+  downloadURLs: {
+    default: string;
+    android?: string;
+    ios?: string;
+  };
 };
 
 export type WalletAdapterType = () => WalletAdapterData;
