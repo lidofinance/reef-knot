24b35629ab318c0dbbf3fad55039b7201e15ad5b
qinghui.tan <qinghui.tan@okg.com>
2023-03-13 11:29:12 +0800

    feature: okx wallet init

--- packages/wallets-list/src/ethereum.ts
+++ packages/wallets-list/src/ethereum.ts
@@ -1,6 +1,8 @@
 import { Exodus } from '@reef-knot/wallet-adapter-exodus';
+import { Okx } from '@reef-knot/wallet-adapter-okx';
 import { WalletsListType } from '@reef-knot/core-react';
 
 export const WalletsListEthereum: WalletsListType = {
   exodus: Exodus,
+  okx: Okx,
 };
