886b71fd5ae061475900f8a9260f768c960c573a
Alexander Khramov <alx.khramov@gmail.com>
2023-04-13 14:03:15 +0300

    fix(web3-react): handle wagmi unsupported chains like web3-react does

--- packages/web3-react/src/hooks/useWeb3.ts
+++ packages/web3-react/src/hooks/useWeb3.ts
@@ -1,6 +1,7 @@
 import { useWeb3React } from '@web3-react/core';
 import { Web3ReactContextInterface } from '@web3-react/core/dist/types';
 import { useAccount, useNetwork, useConnect } from 'wagmi';
+import { useSupportedChains } from './useSupportedChains';
 
 export { UnsupportedChainIdError } from '@web3-react/core';
 
@@ -12,8 +13,29 @@ export function useWeb3<T = any>(key?: string): Web3ReactContextInterface<T> {
   const { error: wagmiError } = useConnect();
 
   const account = web3ReactData.account || wagmiAccount?.address;
-  const active = web3ReactData.active || wagmiAccount?.isConnected;
-  const chainId = web3ReactData.chainId || wagmiNetwork?.chain?.id;
+
+  // web3-react and wagmi have different logic around unsupported chains.
+  // If a chain is not supported:
+  // web3-react sets:
+  //   `chainId` === undefined
+  //   `error` === UnsupportedChainIdError
+  //   `active` === false
+  // wagmi sets:
+  //   `chain.id` === actual value from wallet
+  //   `chain.unsupported` === true
+  //   `error` === null
+  //   `isConnected` === true (connects via default chain)
+  // These differences break our widgets, because the widgets don't expect
+  // chainId to be unsupported, they expect chainId === undefined in that case.
+  // Making wagmi's logic to be the same as web3-react here, except setting an error:
+  const { isUnsupported } = useSupportedChains();
+  const chainId = isUnsupported
+    ? undefined
+    : web3ReactData.chainId || wagmiNetwork?.chain?.id;
+  const active = isUnsupported
+    ? false
+    : web3ReactData.active || wagmiAccount?.isConnected;
+
   // wagmi and web3-react use the same Error type
   // wagmi is first here because it can be null, but we need Error | undefined
   const error = wagmiError || web3ReactData.error;
