7746231a02617c0843b5b2700eb018499d64e453
Alexander Khramov <alx.khramov@gmail.com>
2023-04-18 12:22:43 +0300

    chore: polish OKX adapter

--- packages/wallets/okx/src/index.ts
+++ packages/wallets/okx/src/index.ts
@@ -1,16 +1,39 @@
-import { WalletAdapterType } from '@reef-knot/core-react';
+import { WalletAdapterType } from '@reef-knot/types';
+import { Ethereum as EthereumTypeWagmi } from '@wagmi/core';
+import { InjectedConnector } from 'wagmi/connectors/injected';
 import WalletIcon from './icons/okx.svg';
+import WalletIconInverted from './icons/okx-inverted.svg';
+
+declare module '@wagmi/core' {
+  interface Ethereum {
+    isOkxWallet?: true;
+  }
+}
+
+declare global {
+  interface Window {
+    okxwallet?: EthereumTypeWagmi;
+  }
+}
 
 export const Okx: WalletAdapterType = () => ({
   walletName: 'OKX Wallet',
   walletId: 'okx',
   icons: {
     light: WalletIcon,
-    dark: WalletIcon,
+    dark: WalletIconInverted,
   },
-  detector: () => !!window.okxwallet?.isOkxWallet,
+  detector: () =>
+    !!globalThis.window?.okxwallet?.isOkxWallet ||
+    !!globalThis.window?.ethereum?.isOkxWallet,
   downloadURLs: {
     default: 'https://www.okx.com/download',
   },
-  // TODO: handle wallet conflicts and custom connectors
+  connector: new InjectedConnector({
+    options: {
+      name: 'OKX Wallet',
+      getProvider: () =>
+        globalThis.window?.okxwallet || globalThis.window?.ethereum,
+    },
+  }),
 });
