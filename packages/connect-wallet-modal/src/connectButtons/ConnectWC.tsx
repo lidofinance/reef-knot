35947a0872e9e867eb13e2782ddba1004e5689d0
Alexander Khramov <alx.khramov@gmail.com>
2023-05-22 13:58:07 +0300

    feat(connect-wallet-modal): add WCv2 display_uri handler, misc

--- packages/connect-wallet-modal/src/connectButtons/ConnectWC.tsx
+++ packages/connect-wallet-modal/src/connectButtons/ConnectWC.tsx
@@ -7,8 +7,8 @@ import { ConnectWCProps } from './types';
 
 let redirectionWindow: Window | null = null;
 
-const getRedirectionLoadingHTML = (walletName: string) => {
-  return `<!DOCTYPE html><html lang="en"><head><title>Loading ${walletName}</title><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><body style="display: flex;justify-content: center"><h1 style="font-family: sans-serif">Loading ${walletName}</h1></body></html>`;
+const getRedirectionLoadingHTML = () => {
+  return `<!DOCTYPE html><html lang="en"><head><title>Wallet loading</title><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><body style="display: flex;justify-content: center"><h1 style="font-family: sans-serif">Wallet loading</h1></body></html>`;
 };
 
 const setRedirectionWindowLocation = (redirectLink: string, WCURI: string) => {
@@ -48,13 +48,27 @@ export const ConnectWC: FC<ConnectWCProps> = (props: ConnectWCProps) => {
     walletconnectExtras?.connectionViaURI?.closeRedirectionWindow ?? true;
 
   useEffect(() => {
+    // BEGIN Handle WalletConnect v2 redirection (connection without QR modal)
+    (async () => {
+      const provider = await WCURIConnector?.getProvider();
+      provider?.once('display_uri', (uri: string) => {
+        if (WCURIRedirectLink)
+          setRedirectionWindowLocation(WCURIRedirectLink, uri);
+      });
+    })();
+    // END Handle WalletConnect v2 redirection (connection without QR modal)
+
+    // BEGIN Handle legacy WalletConnect v1 redirection (connection without QR modal)
+    // TODO: remove after migration to v2
     const redirect = async () => {
       if (WCURIConnector && WCURICondition && WCURIRedirectLink) {
-        const WCURI = (await WCURIConnector.getProvider()).connector.uri;
-        setRedirectionWindowLocation(WCURIRedirectLink, WCURI);
+        const WCURI = (await WCURIConnector.getProvider())?.connector?.uri;
+        if (WCURI) setRedirectionWindowLocation(WCURIRedirectLink, WCURI);
       }
     };
     WCURIConnector?.on('message', redirect);
+    // END Handle legacy WalletConnect v1 redirection (connection without QR modal)
+
     return () => {
       WCURIConnector?.off('message', redirect);
     };
@@ -77,7 +91,7 @@ export const ConnectWC: FC<ConnectWCProps> = (props: ConnectWCProps) => {
     if (WCURICondition) {
       // because of popup blockers, window.open must be called directly from onclick handler
       redirectionWindow = window.open('', '_blank');
-      redirectionWindow?.document.write(getRedirectionLoadingHTML(walletName));
+      redirectionWindow?.document.write(getRedirectionLoadingHTML());
       await connectAsync({ connector: WCURIConnector });
       if (WCURICloseRedirectionWindow) {
         redirectionWindow?.close();
@@ -94,7 +108,6 @@ export const ConnectWC: FC<ConnectWCProps> = (props: ConnectWCProps) => {
     disconnect,
     metricsOnClick,
     onBeforeConnect,
-    walletName,
   ]);
 
   const WalletIcon = icon || icons;
