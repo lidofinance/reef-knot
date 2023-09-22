81d751d802ab901f75ef710558c3b43ca89afa14
Alexander Khramov <alx.khramov@gmail.com>
2023-09-22 19:27:50 +0300

    feat: rename bitkeep wallet to bitget, update logo

--- packages/wallets/bitkeep/src/index.ts
+++ packages/wallets/bitkeep/src/index.ts
@@ -1,7 +1,7 @@
 import { WalletAdapterType } from '@reef-knot/types';
 import { Ethereum as EthereumTypeWagmi } from '@wagmi/core';
 import { InjectedConnector } from 'wagmi/connectors/injected';
-import WalletIcon from './icons/bitkeep.svg';
+import WalletIcon from './icons/bitget.svg';
 
 declare global {
   interface Window {
@@ -11,19 +11,25 @@ declare global {
   }
 }
 
-export const BitKeep: WalletAdapterType = ({ chains }) => ({
-  walletName: 'BitKeep',
+export const Bitget: WalletAdapterType = ({ chains }) => ({
+  walletName: 'Bitget',
+  // The current metrics implementation is based on walletId,
+  // using previous "bitkeep" name here not to break metrics
   walletId: 'bitkeep',
   icon: WalletIcon,
   detector: () => !!globalThis.window?.bitkeep?.ethereum,
   downloadURLs: {
-    default: 'https://bitkeep.com/en/download/',
+    default: 'https://web3.bitget.com/',
   },
   connector: new InjectedConnector({
     chains,
     options: {
-      name: 'BitKeep',
+      name: 'Bitget',
       getProvider: () => globalThis.window?.bitkeep?.ethereum,
     },
   }),
 });
+
+// BitKeep is the previous name of the wallet.
+// Exporting the wallet adapter with the old name for compatibility.
+export const BitKeep = Bitget;
