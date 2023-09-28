472ebfa8300f84626d3c6d10c418552122d4460e
Alexander Khramov <alx.khramov@gmail.com>
2023-09-28 14:38:51 +0300

    chore: add prettier to eslint-config-custom

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,29 +1,36 @@
+const { resolve } = require('node:path');
+
+const project = resolve(process.cwd(), 'tsconfig.json');
+
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true,
-    "node": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
-  "extends": [
-    "@lidofinance",
-    "turbo"
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  extends: ['turbo', '@lidofinance', 'prettier'],
+  plugins: ['prettier', '@typescript-eslint', 'react'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": 12,
-    "sourceType": "module",
-    "project": ["tsconfig.json"]
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project,
   },
-  "ignorePatterns": ["*.js", "dist", "node_modules"],
-  "settings": {
-    "react": {
-      "version": "detect"
+  ignorePatterns: ['*.js', 'dist/', 'node_modules/'],
+  settings: {
+    react: {
+      version: 'detect',
     },
-    "import/resolver": {
-      "typescript": {}
-    }
-  }
-}
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+  },
+};
