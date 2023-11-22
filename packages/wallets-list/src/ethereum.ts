0e22bba7a44f75a81d4fb6b010015a844d9a5bb9
Alexander Khramov <alx.khramov@gmail.com>
2023-11-22 18:31:15 +0300

    feat(wallets-list): remove some wallets imports

--- packages/wallets-list/src/ethereum.ts
+++ packages/wallets-list/src/ethereum.ts
@@ -1,13 +1,8 @@
 import { WalletsListType } from '@reef-knot/types';
 import { Exodus } from '@reef-knot/wallet-adapter-exodus';
-import { Taho } from '@reef-knot/wallet-adapter-taho';
 import { Okx } from '@reef-knot/wallet-adapter-okx';
 import { WalletConnect } from '@reef-knot/wallet-adapter-walletconnect';
-import { Blockchaincom } from '@reef-knot/wallet-adapter-blockchaincom';
-import { Zerion } from '@reef-knot/wallet-adapter-zerion';
-import { Zengo } from '@reef-knot/wallet-adapter-zengo';
 import { Ambire } from '@reef-knot/wallet-adapter-ambire';
-import { Phantom } from '@reef-knot/wallet-adapter-phantom';
 import { BitKeep } from '@reef-knot/wallet-adapter-bitkeep';
 import { Coin98 } from '@reef-knot/wallet-adapter-coin98';
 import { Brave } from '@reef-knot/wallet-adapter-brave';
@@ -16,12 +11,7 @@ export const WalletsListEthereum: WalletsListType = {
   okx: Okx,
   walletconnect: WalletConnect,
   exodus: Exodus,
-  taho: Taho,
-  blockchaincom: Blockchaincom,
-  zerion: Zerion,
-  zengo: Zengo,
   ambire: Ambire,
-  phantom: Phantom,
   bitkeep: BitKeep,
   coin98: Coin98,
   brave: Brave,
