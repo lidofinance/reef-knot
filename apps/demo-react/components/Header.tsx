1f47f618e41cb07b72ecc88078ee0ffa4a5815b2
Alexander Khramov <alx.khramov@gmail.com>
2024-02-02 16:55:57 +0300

    feat(demo-react): add favicon and manifest for Safe

--- apps/demo-react/components/Header.tsx
+++ apps/demo-react/components/Header.tsx
@@ -4,6 +4,7 @@ const Header = () => (
   <Head>
     <title>Reef Knot demo app</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <link rel="icon" type="image/png" href="/reef-knot-logo-32x32.png" />
     <style>{`
           body {
             font-family: sans-serif;
