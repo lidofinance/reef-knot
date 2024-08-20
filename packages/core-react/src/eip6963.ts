a4910792c78595151aeb2ef439ade8b4b9f865b1
Alexander Khramov <alx.khramov@gmail.com>
2024-08-20 10:59:58 +0300

    fix: rm unused 'providers' and subscription

--- packages/core-react/src/eip6963.ts
+++ packages/core-react/src/eip6963.ts
@@ -1,5 +1,3 @@
 import { createStore } from 'mipd';
 
 export const providersStore = createStore();
-export let providers = providersStore.getProviders();
-providersStore.subscribe((providerDetails) => (providers = providerDetails));
