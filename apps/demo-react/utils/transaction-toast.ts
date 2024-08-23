119dfb2fb015d84da0ae1b967b02d899aab87a9b
Alexander Khramov <alx.khramov@gmail.com>
2024-08-23 13:58:13 +0300

    fix: disable closeButtons for toasts

--- apps/demo-react/utils/transaction-toast.ts
+++ apps/demo-react/utils/transaction-toast.ts
@@ -7,20 +7,20 @@ import { toast } from '@lidofinance/lido-ui';
 export const transactionToast: TransactionCallback = ({ stage }) => {
   switch (stage) {
     case TransactionCallbackStage.PERMIT:
-      return toast('Permit', { type: 'info' });
+      return toast('Permit', { type: 'info', closeButton: false });
     case TransactionCallbackStage.GAS_LIMIT:
-      return toast('Gas limit', { type: 'info' });
+      return toast('Gas limit', { type: 'info', closeButton: false });
     case TransactionCallbackStage.SIGN:
-      return toast('Signing', { type: 'info' });
+      return toast('Signing', { type: 'info', closeButton: false });
     case TransactionCallbackStage.RECEIPT:
-      return toast('Receipt', { type: 'info' });
+      return toast('Receipt', { type: 'info', closeButton: false });
     case TransactionCallbackStage.CONFIRMATION:
-      return toast('Confirmation', { type: 'success' });
+      return toast('Confirmation', { type: 'success', closeButton: false });
     case TransactionCallbackStage.ERROR:
-      return toast('Error', { type: 'error' });
+      return toast('Error', { type: 'error', closeButton: false });
     case TransactionCallbackStage.DONE:
-      return toast('Success', { type: 'success' });
+      return toast('Success', { type: 'success', closeButton: false });
     case TransactionCallbackStage.MULTISIG_DONE:
-      return toast('Multisig Success', { type: 'success' });
+      return toast('Multisig Success', { type: 'success', closeButton: false });
   }
 };
