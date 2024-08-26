cd665fd91326fcf94a6947223e6d89bfae7e3303
Dmitrii Podlesnyi <rezed93@gmail.com>
2024-08-27 00:48:42 +0700

    fix: binance wallet name

--- packages/wallets/binance-wallet/src/index.ts
+++ packages/wallets/binance-wallet/src/index.ts
@@ -3,7 +3,7 @@ import { WalletIcon } from './icons/index.js';
 import { getWagmiConnectorV2 } from '@binance/w3w-wagmi-connector-v2';
 
 export const id = 'binanceWallet';
-export const name = 'BinanceWeb3Wallet';
+export const name = 'Binance Web3 Wallet';
 
 const binanceWalletConnector = getWagmiConnectorV2();
 
