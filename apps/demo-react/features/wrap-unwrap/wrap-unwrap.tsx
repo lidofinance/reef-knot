4218f0bcb26a73353ef4ebf37fa33f1415de4c1c
Alexander Khramov <alx.khramov@gmail.com>
2024-09-16 22:44:59 +0300

    feat: add Wrap stETH

--- apps/demo-react/features/wrap-unwrap/wrap-unwrap.tsx
+++ apps/demo-react/features/wrap-unwrap/wrap-unwrap.tsx
@@ -8,19 +8,20 @@ import { ActionItem } from 'components';
 import { useInputValueSDK } from 'providers';
 
 enum CallType {
-  wrap = 'wrap',
+  wrapEth = 'wrapEth',
+  wrapSteth = 'wrapSteth',
   unwrap = 'unwrap',
 }
 
 export const WrapUnwrap: FC = () => {
   const { inputValue } = useInputValueSDK();
-  const [type, setType] = useState<CallType>(CallType.wrap);
+  const [type, setType] = useState<CallType>(CallType.wrapEth);
 
   const { account: web3account = '0x0' } = useWeb3();
   const account = web3account as `0x{string}`;
 
   const { wrap } = useLidoSDK();
-  const isWrapTx = type === CallType.wrap;
+  const isWrapTx = type === CallType.wrapEth || type === CallType.wrapSteth;
 
   const handleCall = useCallback(async () => {
     const callValue = inputValue ?? BigInt(0);
@@ -30,9 +31,15 @@ export const WrapUnwrap: FC = () => {
       callback: transactionToast,
     };
 
-    if (isWrapTx) return await wrap.wrapEth(callData);
-    else return await wrap.unwrap(callData);
-  }, [inputValue, account, isWrapTx, wrap]);
+    switch (type) {
+      case CallType.wrapEth:
+        return await wrap.wrapEth(callData);
+      case CallType.wrapSteth:
+        return await wrap.wrapSteth(callData);
+      case CallType.unwrap:
+        return await wrap.unwrap(callData);
+    }
+  }, [inputValue, account, type, wrap]);
 
   const handleTypeChange = (value: string | number) => {
     setType(value as CallType);
@@ -42,7 +49,8 @@ export const WrapUnwrap: FC = () => {
   return (
     <ActionItem title={title} action={handleCall}>
       <Select value={type} onChange={handleTypeChange} fullwidth>
-        <Option value={CallType.wrap}>Wrap</Option>
+        <Option value={CallType.wrapEth}>Wrap ETH</Option>
+        <Option value={CallType.wrapSteth}>Wrap stETH</Option>
         <Option value={CallType.unwrap}>Unwrap</Option>
       </Select>
     </ActionItem>
