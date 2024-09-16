6d41c60fbde19402e7f7a2707441b4bfeb58919a
Alexander Khramov <alx.khramov@gmail.com>
2024-09-16 21:46:06 +0300

    feat: add error boundary

--- apps/demo-react/pages/_app.tsx
+++ apps/demo-react/pages/_app.tsx
@@ -2,6 +2,7 @@ import { memo } from 'react';
 import { AppProps } from 'next/app';
 import { ToastContainer, CookiesTooltip } from '@lidofinance/lido-ui';
 import Providers from 'providers';
+import { ErrorBoundary } from '../components/error-boundary/error-boundary';
 
 const App = (props: AppProps): JSX.Element => {
   const { Component, pageProps } = props;
@@ -13,11 +14,13 @@ const MemoApp = memo(App);
 
 const AppWrapper = (props: AppProps): JSX.Element => {
   return (
-    <Providers>
-      <MemoApp {...props} />
-      <CookiesTooltip />
-      <ToastContainer />
-    </Providers>
+    <ErrorBoundary>
+      <Providers>
+        <MemoApp {...props} />
+        <CookiesTooltip />
+        <ToastContainer />
+      </Providers>
+    </ErrorBoundary>
   );
 };
 
