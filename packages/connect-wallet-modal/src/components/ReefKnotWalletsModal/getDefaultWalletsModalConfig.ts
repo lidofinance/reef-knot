6264e0010d568c891dcbdb69de2093f99f8b9dd2
Alexander Khramov <alx.khramov@gmail.com>
2024-12-11 12:26:13 +0300

    Revert "XDEFI wallet rebranding into Ctrl wallet"

--- packages/connect-wallet-modal/src/components/ReefKnotWalletsModal/getDefaultWalletsModalConfig.ts
+++ packages/connect-wallet-modal/src/components/ReefKnotWalletsModal/getDefaultWalletsModalConfig.ts
@@ -39,7 +39,7 @@ export const WALLETS_MODAL_DISPLAY_CONFIG_DEFAULT: WalletIdsEthereum[] = [
   'exodus',
   'brave',
   'bitget',
-  'ctrl',
+  'xdefi',
   'imToken',
   'coin98',
   'ambire',
--- packages/wallets-list/CHANGELOG.md
+++ packages/wallets-list/CHANGELOG.md
@@ -1,11 +1,5 @@
 # @reef-knot/wallets-list
 
-## 4.1.0
-
-### Minor Changes
-
-- Add Ctrl wallet, rm XDEFI wallet (rebranded into Ctrl)
-
 ## 4.0.0
 
 ### Patch Changes
