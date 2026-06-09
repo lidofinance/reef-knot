fc279d501467bd2853899bd05b2d644997098ad8
Alexander Khramov <alx.khramov@gmail.com>
2026-06-09 12:35:54 +0300

    feat: enhance icon validation with regex check
    Signed-off-by: Alexander Khramov <alx.khramov@gmail.com>

--- packages/connect-wallet-modal/src/helpers/validateIcon.ts
+++ packages/connect-wallet-modal/src/helpers/validateIcon.ts
@@ -1,2 +1,4 @@
+const SAFE_DATA_ICON =
+  /^data:image\/(png|jpeg|jpg|gif|webp);base64,[A-Za-z0-9+/]+=*$/;
 export const validateIcon = (src: string): string | undefined =>
-  src.startsWith('data:') ? src : undefined;
+  SAFE_DATA_ICON.test(src) ? src : undefined;
